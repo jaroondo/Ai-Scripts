240aefd7d8132e6b6fcadbfc6991609e680214b6
Jaroslav Ondo <ondo.jaro@gmail.com>
2022-08-11 12:49:45 +0200

    Inks scripts update

--- InksNameClean.jsx
+++ InksNameClean.jsx
@@ -1,15 +1,15 @@
 // vyhodi PANTONE a C z nazvu farby
 
-(function () {
-  var sps = getDocumentElements("spots");
+var sps = app.activeDocument.swatches;
 
-  // apply REGULAR EXPRESSIONs on page item
-  function clearPantone(item) {
-    item.name = item.name.replace(/PANTONE/, ""); // <- nahradi "PANTONE" prazdnym znakom
-    item.name = item.name.replace(/C$/, ""); // $ = na konci textu <- nahradi "C" prazdnym znakom na konci textu
-    item.name = item.name.replace(/\sC_/, "_"); // \s = space, medzera <- nahradi "C_" znakom "_"
-    item.name = item.name.replace(/\sC\s/, ""); //<- nahradi " C " prazdnym znakom
-  }
+// apply REGULAR EXPRESSIONs on page item
+function clearPantone(item) {
+  item.name = item.name.replace(/PANTONE/, ""); // <- nahradi "PANTONE" prazdnym znakom
+  item.name = item.name.replace(/C$/, ""); // $ = na konci textu <- nahradi "C" prazdnym znakom na konci textu
+  item.name = item.name.replace(/\sC_/, "_"); // \s = space, medzera <- nahradi "C_" znakom "_"
+  item.name = item.name.replace(/\sC\s/, ""); //<- nahradi " C " prazdnym znakom
+}
 
-  forAll(sps, clearPantone);
-})();
+forAll(sps, clearPantone);
+
+app.activeDocument.save();
