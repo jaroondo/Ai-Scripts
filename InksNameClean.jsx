34c27097c87767aefd1332507f1d764c7feede98
Jaroslav Ondo <ondo.jaro@gmail.com>
2022-08-11 10:51:08 +0200

    Update InksNameClean.jsx

--- InksNameClean.jsx
+++ InksNameClean.jsx
@@ -1,35 +1,15 @@
 // vyhodi PANTONE a C z nazvu farby
 
-(function(){
-  var doc = app.activeDocument;
-  //var sws = getDocumentElements("swatches");
+(function () {
   var sps = getDocumentElements("spots");
 
-  function getDocumentElements(setName) { // return array of document items
-    var result = [];
-    
-    for (var i = 0; i < doc[setName].length; i++) {
-      result.push(doc[setName][i]);
-    }
-    
-    return result;
-  }
-
+  // apply REGULAR EXPRESSIONs on page item
   function clearPantone(item) {
-    item.name = item.name.replace(/PANTONE/, "");
-    item.name = item.name.replace(/C$/, ""); // $ na konci textu
-    item.name = item.name.replace(/\sC_/, "_"); // \s space - medzera
-    item.name = item.name.replace(/\sC\s/, "");
+    item.name = item.name.replace(/PANTONE/, ""); // <- nahradi "PANTONE" prazdnym znakom
+    item.name = item.name.replace(/C$/, ""); // $ = na konci textu <- nahradi "C" prazdnym znakom na konci textu
+    item.name = item.name.replace(/\sC_/, "_"); // \s = space, medzera <- nahradi "C_" znakom "_"
+    item.name = item.name.replace(/\sC\s/, ""); //<- nahradi " C " prazdnym znakom
   }
 
-  //sps.forEach(clearPantone); // funguje iba v plugine, v AI hlasi "sps.forEach is not function"
-  
-  function forAll(array, callBack) {
-    for (var i = 0; i < array.length; i++) {
-      callBack(array[i]);
-    }
-  }
-  
   forAll(sps, clearPantone);
-  
 })();
